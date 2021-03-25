f9a7463b3ce9a494bd91b6330dd9d2b5613b9883
jaclynn <jackie@ewilsons.com>
2021-03-25 11:55:37 -0400

    model changes for finding occupancy, first view

--- hotel/models.py
+++ hotel/models.py
@@ -12,18 +12,30 @@ from django.db import models
 ------------------------------------------------------------------------
                                COURTYARD
 '''
+BASE_PRICE = 135.00
+OCEANSIDE_SURCHARGE = 100.00
+SUITE_SURCHARGE = 75.00
+MAXIMUM_STAY = 7  # makes occupied queries easier
 
 
 # Create your models here.
 class Room(models.Model):
     room_num = models.IntegerField(default=0)
-    occupied = models.BooleanField(default=False)
 
     def oceanside(self):
         return self.room_num % 2 == 0
 
     def suite(self):
         return self.room_num % 10 <= 1
+
+    # occupied works better as a calculated field since it involves a date
+    def occupied(self, date):
+        # there may be a better way to do this...
+        bookings = self.booking_set.filter(end__gt=datetime.date.today())
+        for booking in bookings:
+            if date in booking.daterange():
+                return True
+        return False
 
 
 class Guest(models.Model):
@@ -37,6 +49,22 @@ class Booking(models.Model):
     guest = models.ForeignKey(Guest, on_delete=models.CASCADE)
     start = models.DateField(default=datetime.date.today)
     length_of_stay = models.IntegerField(default=1)
+    end = models.DateField(default=datetime.date.today)
 
-    def end(self):
-        return self.start + datetime.timedelta(days=self.length_of_stay)
+    def calc_stay(self):
+        self.end = self.start + datetime.timedelta(days=self.length_of_stay)
+        return self.end
+
+    def daterange(self):
+        return [self.start + datetime.timedelta(days=x) for x in range(1, self.length_of_stay + 1)]
+
+    def price(self):
+        p = BASE_PRICE  # base price
+        if self.room.oceanside():
+            p += OCEANSIDE_SURCHARGE
+        if self.room.suite():
+            p += SUITE_SURCHARGE
+        if self.guest.vip_num > 0:
+            p -= p * 0.8
+        p *= self.length_of_stay
+        return p
